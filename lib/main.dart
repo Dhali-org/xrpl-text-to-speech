190f6b41491ecc9bce96948e99bc39291abf049c
David Simmons <dr.desimmons@gmail.com>
2023-03-27 16:59:33 +0100

    Add styling

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,10 @@ class TextInputScreenState extends State<TextInputScreen> {
     return Scaffold(
         body: Column(children: [
           // If the Future is complete, display the preview.
-          getHeader(),
+          Spacer(flex: 1),
+          Expanded(child: getHeader(), flex: 3),
+          Spacer(flex: 10),
+
           _wallet == null
               ? SelectableText('Please activate your wallet!',
                   style: const TextStyle(fontSize: 25))
@@ -81,157 +84,175 @@ class TextInputScreenState extends State<TextInputScreen> {
                             CircularProgressIndicator()
                           ]);
                     }
-                    return Column(
-                      children: [
-                        SelectableText('Classic address: ${_wallet!.address}',
-                            style: const TextStyle(fontSize: 25)),
-                        SelectableText('Balance: $balance XRP',
-                            style: const TextStyle(fontSize: 25)),
-                      ],
-                    );
+                    return Row(children: [
+                      Spacer(flex: 2),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SelectableText('Classic address: ${_wallet!.address}',
+                              style: const TextStyle(fontSize: 25)),
+                          SelectableText('Balance: $balance XRP',
+                              style: const TextStyle(fontSize: 25)),
+                        ],
+                      ),
+                      Spacer(flex: 10),
+                    ]);
                   }),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Lorem ipsum dolor sit amet',
+          Spacer(flex: 3),
+          Container(
+            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+            child: TextField(
+              maxLines: 10,
+              minLines: 10,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Please enter the text to be spoken here',
+              ),
+              controller: _submissionTextController,
             ),
-            controller: _submissionTextController,
           ),
+          Spacer(flex: 5),
         ]),
-        floatingActionButton: Stack(
-          fit: StackFit.expand,
+        floatingActionButton: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Positioned(
-                left: 30,
-                bottom: 20,
-                child: FloatingActionButton(
-                  heroTag: "run",
-                  tooltip: "Run inference",
-                  onPressed: () async {
-                    if (_wallet == null) {
-                      showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: const Text('Invalid wallet'),
-                          content: const Text('Please activate your wallet!'),
-                          actions: [
-                            ElevatedButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('OK'))
-                          ],
-                        ),
-                      );
-                      return;
-                    }
-
-                    updateSnackBar(snackBarType: SnackBarTypes.inProgress);
-
-                    // Take the Picture in a try / catch block. If anything goes wrong,
-                    // catch the error.
-                    try {
-                      if (!mounted) return;
-                      String dest =
-                          "rstbSTpPcyxMsiXwkBxS9tFTrg2JsDNxWk"; // Dhali's address
-                      String amount =
-                          "10000000"; // The total amount escrowed in the channel
-                      String authAmount = _wallet!.balance
-                          .value!; // The amount to authorise for the claim
-                      var openChannels = await _wallet!
-                          .getOpenPaymentChannels(destination_address: dest);
-                      if (openChannels.isEmpty) {
-                        openChannels = [
-                          await _wallet!.openPaymentChannel(dest, amount)
-                        ];
-                      }
-                      Map<String, String> paymentClaim = {
-                        "account": _wallet!.address,
-                        "destination_account": dest,
-                        "authorized_to_claim": authAmount,
-                        "signature": _wallet!
-                            .sendDrops(authAmount, openChannels[0].channelId),
-                        "channel_id": openChannels[0].channelId
-                      };
-                      Map<String, String> header = {
-                        "Payment-Claim":
-                            const JsonEncoder().convert(paymentClaim)
-                      };
-                      String entryPointUrlRoot = _endPoint;
-
-                      var request = http.MultipartRequest(
-                          "PUT", Uri.parse(entryPointUrlRoot));
-                      request.headers.addAll(header);
-
-                      var logger = Logger();
-                      logger.d("Preparing file in body");
-                      var textBytes = _submissionTextController.text.codeUnits;
-                      request.files.add(http.MultipartFile(
-                          contentType: MediaType('multipart', 'form-data'),
-                          "input",
-                          Stream.value(textBytes),
-                          textBytes.length,
-                          filename: "input"));
-
-                      var finalResponse = await request.send();
-                      logger.d("Status: ${finalResponse.statusCode}");
-                      var response = json
-                          .decode(await finalResponse.stream.bytesToString());
-
-                      if (finalResponse.statusCode == 200) {
-                        updateSnackBar(snackBarType: SnackBarTypes.success);
-                        final audioContext = AudioContext();
-
-                        try {
-                          List<double> audioSamples =
-                              response["results"].cast<double>();
-
-                          final audioContext = AudioContext();
-                          final audioBuffer = audioContext.createBuffer(
-                              1, audioSamples.length, 16000);
-
-                          // Fill the buffer with the audio samples
-                          Float32List buffer =
-                              Float32List.fromList(audioSamples);
-                          audioBuffer.copyToChannel(buffer, 0);
-
-                          // Create a buffer source and connect it to the destination
-                          final audioBufferSource =
-                              audioContext.createBufferSource();
-                          audioBufferSource.buffer = audioBuffer;
-                          audioBufferSource
-                              .connectNode(audioContext.destination!);
-
-                          audioBufferSource.start(0);
-                        } catch (e, stacktrace) {
-                          print('Error playing audio: $e');
-                          print('Stack: ${stacktrace}');
-                        }
-                      } else {
-                        updateSnackBar(
-                            message: response.toString(),
-                            snackBarType: SnackBarTypes.error);
-                      }
-                    } catch (e) {
-                      updateSnackBar(snackBarType: SnackBarTypes.error);
-                    } finally {
-                      Future.delayed(const Duration(milliseconds: 1000), () {
-                        setState(() {
-                          updateSnackBar();
-                        });
-                      });
-                    }
-                  },
-                  child: const Icon(Icons.speaker_notes),
-                )),
-            Positioned(
-              bottom: 20,
-              right: 30,
-              child: getWalletFloatingActionButton("Top-up balance"),
+            Spacer(flex: 1),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Cost", style: const TextStyle(fontSize: 25)),
+                Text("GitHub repo", style: const TextStyle(fontSize: 25)),
+                Text("Cost", style: const TextStyle(fontSize: 25))
+              ],
             ),
+            Spacer(flex: 8),
+            getWalletFloatingActionButton("Top-up balance"), Spacer(flex: 1),
+            getInferenceFloatingActionButton(), Spacer(flex: 1)
+
 // Add more floating buttons if you want
           ],
         ));
+  }
+
+  Widget getInferenceFloatingActionButton() {
+    return FloatingActionButton(
+      heroTag: "run",
+      tooltip: "Run inference",
+      onPressed: () async {
+        if (_wallet == null) {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text('Invalid wallet'),
+              content: const Text('Please activate your wallet!'),
+              actions: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('OK'))
+              ],
+            ),
+          );
+          return;
+        }
+
+        updateSnackBar(snackBarType: SnackBarTypes.inProgress);
+
+        // Take the Picture in a try / catch block. If anything goes wrong,
+        // catch the error.
+        try {
+          if (!mounted) return;
+          String dest = "rstbSTpPcyxMsiXwkBxS9tFTrg2JsDNxWk"; // Dhali's address
+          String amount =
+              "10000000"; // The total amount escrowed in the channel
+          String authAmount =
+              _wallet!.balance.value!; // The amount to authorise for the claim
+          var openChannels =
+              await _wallet!.getOpenPaymentChannels(destination_address: dest);
+          if (openChannels.isEmpty) {
+            openChannels = [await _wallet!.openPaymentChannel(dest, amount)];
+          }
+          Map<String, String> paymentClaim = {
+            "account": _wallet!.address,
+            "destination_account": dest,
+            "authorized_to_claim": authAmount,
+            "signature":
+                _wallet!.sendDrops(authAmount, openChannels[0].channelId),
+            "channel_id": openChannels[0].channelId
+          };
+          Map<String, String> header = {
+            "Payment-Claim": const JsonEncoder().convert(paymentClaim)
+          };
+          String entryPointUrlRoot = _endPoint;
+
+          var request =
+              http.MultipartRequest("PUT", Uri.parse(entryPointUrlRoot));
+          request.headers.addAll(header);
+
+          var logger = Logger();
+          logger.d("Preparing file in body");
+          var textBytes = _submissionTextController.text.codeUnits;
+          request.files.add(http.MultipartFile(
+              contentType: MediaType('multipart', 'form-data'),
+              "input",
+              Stream.value(textBytes),
+              textBytes.length,
+              filename: "input"));
+
+          var finalResponse = await request.send();
+          logger.d("Status: ${finalResponse.statusCode}");
+          var response =
+              json.decode(await finalResponse.stream.bytesToString());
+
+          if (finalResponse.statusCode == 200) {
+            updateSnackBar(snackBarType: SnackBarTypes.success);
+            final audioContext = AudioContext();
+
+            try {
+              List<double> audioSamples = response["results"].cast<double>();
+
+              final audioContext = AudioContext();
+              final audioBuffer =
+                  audioContext.createBuffer(1, audioSamples.length, 16000);
+
+              // Fill the buffer with the audio samples
+              Float32List buffer = Float32List.fromList(audioSamples);
+              audioBuffer.copyToChannel(buffer, 0);
+
+              // Create a buffer source and connect it to the destination
+              final audioBufferSource = audioContext.createBufferSource();
+              audioBufferSource.buffer = audioBuffer;
+              audioBufferSource.connectNode(audioContext.destination!);
+
+              audioBufferSource.start(0);
+            } catch (e, stacktrace) {
+              print('Error playing audio: $e');
+              print('Stack: ${stacktrace}');
+            }
+          } else {
+            updateSnackBar(
+                message: response.toString(),
+                snackBarType: SnackBarTypes.error);
+          }
+        } catch (e) {
+          updateSnackBar(snackBarType: SnackBarTypes.error);
+        } finally {
+          Future.delayed(const Duration(milliseconds: 1000), () {
+            setState(() {
+              updateSnackBar();
+            });
+          });
+        }
+      },
+      child: const Icon(
+        Icons.play_arrow,
+        size: 40,
+        fill: 1,
+      ),
+    );
   }
 
   Widget getWalletFloatingActionButton(String text) {
@@ -329,32 +350,8 @@ class TextInputScreenState extends State<TextInputScreen> {
           Expanded(child: getHeader(), flex: 3),
           Spacer(flex: 10),
           Expanded(
-              child: _wallet == null
-                  ? SelectableText('Please activate your wallet!',
-                      style: const TextStyle(fontSize: 25))
-                  : ValueListenableBuilder<String?>(
-                      valueListenable: _wallet!.balance,
-                      builder:
-                          (BuildContext context, String? balance, Widget? _) {
-                        if (balance == null) {
-                          return Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text("Loading wallet: ",
-                                    style: TextStyle(fontSize: 25)),
-                                CircularProgressIndicator()
-                              ]);
-                        }
-                        return Column(
-                          children: [
-                            SelectableText(
-                                'Classic address: ${_wallet!.address}',
-                                style: const TextStyle(fontSize: 25)),
-                            SelectableText('Balance: $balance XRP',
-                                style: const TextStyle(fontSize: 25)),
-                          ],
-                        );
-                      }),
+              child: SelectableText('Please activate your wallet!',
+                  style: const TextStyle(fontSize: 25)),
               flex: 10),
         ]),
         floatingActionButton: getWalletFloatingActionButton("Get wallet"));
